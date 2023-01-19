5c04661ec16909e5a5cca4fbb10e09b6b865d4d3
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-01-18 22:40:32 -0500

    Add use effects to main application component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -35,26 +35,54 @@ export default class Dashboard extends Component {
 
   state = {
     loading: false,
-    data
+    data,
+    focused: null
   };
 
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+
+  selectPanel = (id) => {
+
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
+  
+  }
+
   render() { // Function to render components
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+     });
 
     if (this.state.loading) {  // Why do we need to return each component?
       return <Loading/>  
     }
 
     
-    const panels = data.map((panel) => {
-        return(
-          <Panel
-            key={panel.id}
-            id={panel.id}
-            label={panel.label}
-            value={panel.value}
-          />
-        );
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+    .map((panel) => {
+      return(
+        <Panel
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={event => this.selectPanel(panel.id)}
+        />
+      );
     });
 
     return (
